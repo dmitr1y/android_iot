b3334097ad679b9aae36547309d0212a469f5a21
dmitriy <aser00707@ya.ru>
2016-11-29 00:58:02 +0300

    added javadoc

---
+++ app/src/test/java/com/kd/dmitriy/arduinoiot/MainActivityTest.java
@@ -0,0 +1,22 @@
+package com.kd.dmitriy.arduinoiot;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dmitriy on 29.11.16.
+ */
+public class MainActivityTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
